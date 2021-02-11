cafd1fd65988b34f859789c6ec08da99cac4246d
Hannes <hasnep@gmail.com>
2021-02-11 19:04:02 +0000

    Move chord parsing to separate file

---
+++ src/chords.rs
@@ -0,0 +1,139 @@
+use regex::Regex;
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq, Eq)]
+enum Note {
+    Af,
+    A,
+    As,
+    Bf,
+    B,
+    Bs,
+    Cf,
+    C,
+    Cs,
+    Df,
+    D,
+    Ds,
+    Ef,
+    E,
+    Es,
+    Ff,
+    F,
+    Fs,
+    Gf,
+    G,
+    Gs,
+}
+
+impl FromStr for Note {
+    type Err = ();
+    fn from_str(input: &str) -> Result<Note, Self::Err> {
+        match input {
+            "Ab" => Ok(Note::Af),
+            "A" => Ok(Note::A),
+            "A#" => Ok(Note::As),
+            "Bb" => Ok(Note::Bf),
+            "B" => Ok(Note::B),
+            "B#" => Ok(Note::Bs),
+            "Cb" => Ok(Note::Cf),
+            "C" => Ok(Note::C),
+            "C#" => Ok(Note::Cs),
+            "Db" => Ok(Note::Df),
+            "D" => Ok(Note::D),
+            "D#" => Ok(Note::Ds),
+            "Eb" => Ok(Note::Ef),
+            "E" => Ok(Note::E),
+            "E#" => Ok(Note::Es),
+            "Fb" => Ok(Note::Ff),
+            "F" => Ok(Note::F),
+            "F#" => Ok(Note::Fs),
+            "Gb" => Ok(Note::Gf),
+            "G" => Ok(Note::G),
+            "G#" => Ok(Note::Gs),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Chord {
+    root: Note,
+    extension: String,
+    bass: Option<Note>,
+}
+
+pub fn parse_chord(chord: &str) -> Chord {
+    // Make sure the regex pattern is only compiled once
+    lazy_static! {
+        static ref RE: Regex =
+            Regex::new(r"^(?P<root>[A-G][#b]?)(?P<extension>.*?)(?:/(?P<bass>[A-G][#b]?))?$")
+                .unwrap();
+    }
+    let caps = RE.captures(chord).unwrap();
+    return Chord {
+        root: Note::from_str(caps.name("root").unwrap().as_str()).unwrap(),
+        extension: caps.name("extension").unwrap().as_str().to_string(),
+        bass: match caps.name("bass") {
+            Some(v) => Note::from_str(v.as_str()).ok(),
+            _ => None,
+        },
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_parse_chord() {
+        let test_cases = vec![
+            (
+                "C",
+                Chord {
+                    root: Note::C,
+                    extension: "".to_string(),
+                    bass: None,
+                },
+            ),
+            (
+                "C#",
+                Chord {
+                    root: Note::Cs,
+                    extension: "".to_string(),
+                    bass: None,
+                },
+            ),
+            (
+                "D7",
+                Chord {
+                    root: Note::D,
+                    extension: "7".to_string(),
+                    bass: None,
+                },
+            ),
+            (
+                "Eb/C",
+                Chord {
+                    root: Note::Ef,
+                    extension: "".to_string(),
+                    bass: Some(Note::C),
+                },
+            ),
+            (
+                "Emaj7/C#",
+                Chord {
+                    root: Note::E,
+                    extension: "maj7".to_string(),
+                    bass: Some(Note::Cs),
+                },
+            ),
+        ];
+        for (input, expected_output) in test_cases {
+            assert_eq!(parse_chord(input).root, expected_output.root);
+            assert_eq!(parse_chord(input).extension, expected_output.extension);
+            assert_eq!(parse_chord(input).bass, expected_output.bass);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 #[macro_use]
 extern crate lazy_static;
 
-use regex::Regex;
 use serde_derive::Deserialize;
-use std::fs;
-use std::str::FromStr;
-use std::str::Lines;
 use std::env;
+use std::fs;
+
+use std::str::Lines;
 use toml;
+
+mod chords;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -63,68 +64,6 @@ fn split_frontmatter_body(lines: Lines) -> (usize, usize) {
     }
 }
 
-#[derive(Debug,PartialEq, Eq)]
-enum Note {
-    Af,
-    A,
-    As,
-    Bf,
-    B,
-    Bs,
-    Cf,
-    C,
-    Cs,
-    Df,
-    D,
-    Ds,
-    Ef,
-    E,
-    Es,
-    Ff,
-    F,
-    Fs,
-    Gf,
-    G,
-    Gs,
-}
-
-impl FromStr for Note {
-    type Err = ();
-    fn from_str(input: &str) -> Result<Note, Self::Err> {
-        match input {
-            "Ab" => Ok(Note::Af),
-            "A" => Ok(Note::A),
-            "A#" => Ok(Note::As),
-            "Bb" => Ok(Note::Bf),
-            "B" => Ok(Note::B),
-            "B#" => Ok(Note::Bs),
-            "Cb" => Ok(Note::Cf),
-            "C" => Ok(Note::C),
-            "C#" => Ok(Note::Cs),
-            "Db" => Ok(Note::Df),
-            "D" => Ok(Note::D),
-            "D#" => Ok(Note::Ds),
-            "Eb" => Ok(Note::Ef),
-            "E" => Ok(Note::E),
-            "E#" => Ok(Note::Es),
-            "Fb" => Ok(Note::Ff),
-            "F" => Ok(Note::F),
-            "F#" => Ok(Note::Fs),
-            "Gb" => Ok(Note::Gf),
-            "G" => Ok(Note::G),
-            "G#" => Ok(Note::Gs),
-            _ => Err(()),
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct Chord {
-    root: Note,
-    extension: String,
-    bass: Option<Note>,
-}
-
 // fn parse_body(body: Vec<str>) {
 //     for line in body {}
 // }
@@ -167,79 +106,4 @@ struct Section {
 struct Line {
     chords: Vec<String>,
     lyrics: Vec<String>,
-}
-
-pub fn parse_chord(chord: &str) -> Chord {
-    // Make sure the regex pattern is only compiled once
-    lazy_static! {
-        static ref RE: Regex =
-            Regex::new(r"^(?P<root>[A-G][#b]?)(?P<extension>.*?)(?:/(?P<bass>[A-G][#b]?))?$")
-                .unwrap();
-    }
-    let caps = RE.captures(chord).unwrap();
-    return Chord {
-        root: Note::from_str(caps.name("root").unwrap().as_str()).unwrap(),
-        extension: caps.name("extension").unwrap().as_str().to_string(),
-        bass: match caps.name("bass") {
-            Some(v) => Note::from_str(v.as_str()).ok(),
-            _ => None,
-        },
-    };
-}
-
-#[cfg(test)]
-mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
-    use super::*;
-
-    #[test]
-    fn test_parse_chord() {
-        let test_cases = vec![
-            (
-                "C",
-                Chord {
-                    root: Note::C,
-                    extension: "".to_string(),
-                    bass: None,
-                },
-            ),
-            (
-                "C#",
-                Chord {
-                    root: Note::Cs,
-                    extension: "".to_string(),
-                    bass: None,
-                },
-            ),
-            (
-                "D7",
-                Chord {
-                    root: Note::D,
-                    extension: "7".to_string(),
-                    bass: None,
-                },
-            ),
-            (
-                "Eb/C",
-                Chord {
-                    root: Note::Ef,
-                    extension: "".to_string(),
-                    bass: Some(Note::C),
-                },
-            ),
-            (
-                "Emaj7/C#",
-                Chord {
-                    root: Note::E,
-                    extension: "maj7".to_string(),
-                    bass: Some(Note::Cs),
-                },
-            ),
-        ];
-        for (input, expected_output) in test_cases {
-            assert_eq!(parse_chord(input).root, expected_output.root);
-            assert_eq!(parse_chord(input).extension, expected_output.extension);
-            assert_eq!(parse_chord(input).bass, expected_output.bass);
-        }
-    }
 }
